a5c367c78234fd0253e312a551992f6ffd15697d
AbeeraUmair <147932381+AbeeraUmair@users.noreply.github.com>
2024-03-08 13:09:51 +0500

    Add files via upload

---
+++ Eid list Assignment/app.js
@@ -0,0 +1,41 @@
+getul = document.getElementById('ul')
+
+function foo() {
+    var a = document.getElementById('inp')
+    var li = document.createElement('li')
+    var liText = document.createTextNode(a.value)
+    getul.appendChild(li)
+    li.appendChild(liText)
+    a.valuE = ''
+    li.setAttribute('class', 'li')
+
+    var deletebtn = document.createElement('button')
+    var deletebtnText = document.createTextNode('delete')
+    deletebtn.appendChild(deletebtnText)
+    li.appendChild(deletebtn)
+    deletebtn.setAttribute('onclick', 'dell(this)')
+    deletebtn.setAttribute('class', 'delete btn btn-danger  ')
+
+
+    var editbtn = document.createElement('button')
+    var editbtnText = document.createTextNode('edit')
+    editbtn.appendChild(editbtnText)
+    li.appendChild(editbtn)
+    editbtn.setAttribute('onclick', 'edit(this)')
+    editbtn.setAttribute('class', 'edit btn btn-info ')
+
+    console.log(li)
+}
+
+function dell(e) {
+    e.parentNode.remove()
+}
+
+function edit(e) {
+    var a = prompt('enter edit value', e.parentNode.firstChild.nodeValue)
+    e.parentNode.firstChild.nodeValue = a
+}
+
+function del() {
+    getul.innerHTML = ''
+}
